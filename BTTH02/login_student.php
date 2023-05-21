d78f7cf4d1ea08aada6008f4d02977b21f381b1a
NguyenKhanhLinh562 <2051060584@e.tlu.edu.vn>
2023-05-21 10:20:22 +0700

    Edit Database

---
+++ BTTH02/login_student.php
@@ -0,0 +1,90 @@
+<?php
+session_start();
+
+// Kiểm tra nếu người dùng đã đăng nhập
+if (isset($_SESSION['student_id'])) {
+    header("Location: index.php"); // Chuyển hướng đến trang chính đã đăng nhập
+}
+
+// Xử lý đăng nhập
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $studentId = $_POST['student_id'];
+    $password = $_POST['password'];
+
+    // Kiểm tra thông tin đăng nhập (có thể sử dụng cơ sở dữ liệu để xác thực)
+    if ($studentId === "Linh" && $password === "12345") {
+        $_SESSION['student_id'] = $studentId;
+        header("Location: index_student.php"); // Chuyển hướng đến trang chính sau khi đăng nhập thành công
+    } else {
+        $error = "Thông tin đăng nhập không chính xác";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Đăng nhập</title>
+    <style>
+        .container {
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        .form-group {
+            margin-bottom: 15px;
+        }
+
+        .form-group label {
+            display: block;
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+
+        .form-group input {
+            width: 100%;
+            padding: 10px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+        }
+
+        .form-group .error {
+            color: red;
+        }
+
+        .form-group .submit-btn {
+            width: auto;
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h2>Đăng nhập</h2>
+        <?php if (isset($error)) : ?>
+            <p class="error"><?php echo $error; ?></p>
+        <?php endif; ?>
+        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <div class="form-group">
+                <label for="student_id">Mã sinh viên:</label>
+                <input type="text" id="student_id" name="student_id" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Mật khẩu:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Đăng nhập" class="submit-btn">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
